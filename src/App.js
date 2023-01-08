be171fe4dc9fc62e4e34479909eda54c0c4a857a
Okellothomas <thomasolal33@gmail.com>
2023-01-08 17:51:05 +0300

    add the coursel component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import NavBar from "./component/Navbar";
 import Home from "./pages/Home";
 // import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
-
 export default function App() {
     return (
         <div>
             <NavBar />
             <Home />
-            {/* <Router>
-            <Switch>
-            <Route>
-                        
+            {/* <BrowserRouter>
+            <Router>
+            
+            <Switch> */}
+            {/* <Route path="/"> 
+            <Home />                
             </Route>
             </Switch>
-            </Router> */}
+            </Router>
+            </BrowserRouter> */}
         </div>
     )
 }
