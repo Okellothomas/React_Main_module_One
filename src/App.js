834050194cf7fc78a4d0bbd2226b782b768aa237
Okellothomas <thomasolal33@gmail.com>
2023-01-08 17:17:54 +0300

    add the router to the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import NavBar from "./component/Navbar";
+import Home from "./pages/Home";
+// import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
     return (
         <div>
             <NavBar />
+            <Home />
+            {/* <Router>
+            <Switch>
+            <Route>
+                        
+            </Route>
+            </Switch>
+            </Router> */}
         </div>
     )
 }
